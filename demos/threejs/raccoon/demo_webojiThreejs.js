e89bc5cec2c49b4b49cd87564676fa8ca4478701
rkk16 <rkk16@ic.ac.uk>
2020-04-13 21:56:13 +0100

    Update demo_webojiThreejs.js

--- demos/threejs/raccoon/demo_webojiThreejs.js
+++ demos/threejs/raccoon/demo_webojiThreejs.js
@@ -10,7 +10,7 @@ function main(){
     NNCpath: '../../../dist/',
 
     //FOX :
-    meshURL: 'meshes/fox11_v0.json',
+    meshURL: 'meshes/FaceRec.json',
     matParameters: {
       diffuseMapURL: 'textures/Fox_albedo.png',
       specularMapURL: 'textures/Fox_specular.png',
